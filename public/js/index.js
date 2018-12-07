99970cbbd339206258383b64db1544c878912eff
Scott Ritchey <scottritchey@Scotts-MacBook-Air-2.local>
2018-12-06 21:10:06 -0500

    Updates to view, index.js, html routes

--- public/js/index.js
+++ public/js/index.js
@@ -1,70 +1,105 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+
+// Get references to page elements and set variables for all idvidual data that needs to be stored
+var allergyGluten = $("#gluten");
+var allergyShellfish = $("#shellfish");
+var allergyPeanuts = $("#peanuts");
+var allergyNutsOther = $("#nutsOther");
+var allergydairy = $("#dairy");
+var allergyEggs = $("#eggs");
+var cuisineType = $("#cuisineType");
+var courseType = $("#courseType")
+var recipeIngredient = $("#textAreaIngredient");
+var recipeAmount = $("#textAreaAmount");
+var measureSolids = $("#measureSolids");
+var measureLiquids = $("#measureLiquids");
+var recipeInstructions = $("#textAreaInstructions");
+var submitBtn = $("#submitBtn");
+var privateBtn = $("#privateBtn");
+var recipeList = $("#recipeList");
+
+
 
 // The API object contains methods for each kind of request we'll make
+// URL empty until html routes determined
 var API = {
-  saveExample: function(example) {
+  saveRecipe: function(recipe) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
       },
       type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
+      url: "",
+      data: JSON.stringify(saveRecipe)
     });
   },
-  getExamples: function() {
+  getRecipe: function() {
     return $.ajax({
-      url: "api/examples",
+      url: "",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  deleteRecipe: function(id) {
     return $.ajax({
-      url: "api/examples/" + id,
+      url: "" + id,
       type: "DELETE"
     });
   }
 };
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+// searchRecipes gets new recipes from the db and repopulates the list
+var searchRecipe = function() {
+  API.getRecipe().then(function(data) {
+    var recipe = data.map(function(recipe) {
+      var searchedRecipeHolder = $("<#searchedRecipeHolder>")
+        .text(recipe.text)
+        .attr("href", "/example/" + recipe.id);
 
-      var $li = $("<li>")
+      var li = $("<li>")
         .attr({
           class: "list-group-item",
-          "data-id": example.id
+          "data-id": recipe.id
         })
-        .append($a);
+        .append(searchedRecipeHolder);
 
-      var $button = $("<button>")
+      var recipeDeleteButton = $("<button>")
         .addClass("btn btn-danger float-right delete")
         .text("ｘ");
 
-      $li.append($button);
+      li.append(recipeDeleteButton);
 
-      return $li;
+      return li;
     });
 
-    $exampleList.empty();
-    $exampleList.append($examples);
+    recipeList.empty();
+   recipeList.append(recipes);
   });
 };
+// Function that generates rows in materialize
+$(document).ready(function () {
+  $('select').formSelect();
+});
+
+// Function that generates new ingredient row
+$(document).ready(function(){
+  console.log("Running?");
+$("#add-button").click(function() {
+  console.log("click event?");
+    var ingredientRow = document.getElementById("ingredientRow").outerHTML;
+    $("#hide").append(ingredientRow);
+    console.log(ingredientRow);
+  });
+
+});
+
+
+
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
 var handleFormSubmit = function(event) {
   event.preventDefault();
 
-  var example = {
+  var submitRecipe = {
     text: $exampleText.val().trim(),
     description: $exampleDescription.val().trim()
   };
@@ -95,5 +130,7 @@ var handleDeleteBtnClick = function() {
 };
 
 // Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+submitBtn.on("click", handleFormSubmit);
+//exampleList.on("click", ".delete", handleDeleteBtnClick);
+//addIngredient.on("click", handleRowExpand);
+
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,23 +1,20 @@
 var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../layouts/login.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Load search page
+  app.get("/search", function(req, res) {
+    res.sendFile(path.join(__dirname, "../layouts/search.html"));
+  });
+
+  // Load submit page
+  app.get("/submit", function(req, res) {
+    res.sendFile(path.join(__dirname, "../layouts/submit.html"));
   });
 
   // Render 404 page for any unmatched routes
