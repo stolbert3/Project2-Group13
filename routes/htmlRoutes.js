09555d3c3997302d1c9cee6ea17144400a3f3a33
Sydney <stolbert3@gatech.edu>
2018-12-11 15:41:11 -0500

    updates to js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ var path = require("path");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/layouts/login.html"));
+    res.sendFile(path.join(__dirname, "../views/layouts/index.html"));
   });
 
   // Load search page
