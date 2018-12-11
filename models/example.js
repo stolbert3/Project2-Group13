80a1d082c2b7841f8c6699b0d88254c79a53e32d
Sydney <stolbert3@gatech.edu>
2018-12-11 12:11:24 -0500

    adjusted folder layout

--- models/example.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var recipes = sequelize.define("recipes", {
-    id: DataTypes.INT,
-    recipe_name: DataTypes.TEXT,
-    description: DataTypes.TEXT
-  });
-  return recipes;
-};
