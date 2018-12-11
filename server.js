bd302547de893a7a0164057bc022dcf0e4e89871
Sydney <stolbert3@gatech.edu>
2018-12-11 18:05:37 -0500

    fixing connections

--- server.js
+++ server.js
@@ -10,17 +10,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(__dirname + '/views'));
+app.use(express.static(path.join(__dirname, '/views')))
 
-// Handlebars
-/*
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-*/
 
 // Routes
 require("./routes/apiRoutes")(app);
